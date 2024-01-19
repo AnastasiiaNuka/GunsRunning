3d2b06d848a7462dea5b96e8cac188e02a59d1f0
AnastasiiaNuka <nastianuka97@gmail.com>
2024-01-19 19:43:30 +0100

    Added some assets for scene1

--- Assets/scripts/CameraFollow.cs
+++ Assets/scripts/CameraFollow.cs
@@ -6,17 +6,17 @@ public class CameraFollow : MonoBehaviour
 {
 
     [SerializeField] float minX, maxX, minY, maxY;
-    [SerializeField] Transform target;
+    [SerializeField] Transform target;//за ним следует камера
 
     [SerializeField] float followSpeed;
 
     void FixedUpdate()
     {
-        if (!target) return;// если таргета нет
+        if (!target) return;// если таргета нет то не выполняем
        
         transform.position = Vector3.Lerp(transform.position, new Vector3(Mathf.Clamp(target.position.x, minX, maxX), Mathf.Clamp(target.position.y, minY, maxY), -10), followSpeed * Time.fixedDeltaTime);
         //transform.position = new Vector3(Mathf.Clamp(target.position.x, minX, maxX), Mathf.Clamp(target.position.y, minY, maxY), -10);//камера позади всех обьектов и все были в сцене
-
+        //линейная интерпалаяция плавное значение от позиции камера и игрока в заданных промежутках и скорость чтоб на разных устройствах одна скорость
 
     }
 }
