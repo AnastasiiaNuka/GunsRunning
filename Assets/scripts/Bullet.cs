d7a50718635efd42afa2b1be78ec61b8c8bac8d3
AnastasiiaNuka <nastianuka97@gmail.com>
2024-01-19 18:10:21 +0100

    Added bullet script

--- Assets/scripts/Bullet.cs
+++ Assets/scripts/Bullet.cs
@@ -4,19 +4,36 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    [SerializeField] float speed; 
+    [SerializeField] float speed;
+    [SerializeField] float deathTime;
 
 
 
 
     void Start()
     {
-        
+        Invoke(nameof(Death), deathTime); //вызов функции через время
     }
 
-   
+
     void Update()
     {
-        transform.Translate(Vector3.right* Time.deltaTime*speed); //перемещение пули
+        transform.Translate(Vector3.right * Time.deltaTime * speed); //перемещение пули
     }
-}
+
+    private void OnTriggerEnter2D(Collider2D collision) //когда столкнулся распознает с другим тригером
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            Death();
+         }
+
+
+    }
+
+        void Death()
+        {
+            Destroy(gameObject);
+        }
+    }
+
